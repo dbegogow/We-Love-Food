6ad045b25f899f70702170c4a3d4ae07a18c0bc5
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-04 13:50:28 +0300

    Implement Image slider in home page

---
+++ WeLoveFood/WeLoveFood/wwwroot/js/index.js
@@ -0,0 +1,36 @@
+﻿(() => {
+    const images = document.getElementsByClassName("slider-image");
+    const buttons = document.getElementsByClassName("slider-btn");
+
+    let index = 1;
+    let repeatLoop = false;
+
+    const activeClass = 'active';
+
+    setInterval(() => {
+        if (repeatLoop) {
+            images[4].classList.remove(activeClass);
+            buttons[4].classList.remove(activeClass);
+
+            images[0].classList.add(activeClass);
+            buttons[0].classList.add(activeClass);
+
+            repeatLoop = false;
+
+            return;
+        }
+
+        images[index - 1].classList.remove(activeClass);
+        buttons[index - 1].classList.remove(activeClass);
+
+        images[index].classList.add(activeClass);
+        buttons[index].classList.add(activeClass);
+
+        index++;
+
+        if (index > 4) {
+            index = 1;
+            repeatLoop = true;
+        }
+    }, 2000);
+})();

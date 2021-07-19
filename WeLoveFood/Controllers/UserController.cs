5796b83ab3ed2c6525433b46e00f1608689984f4
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-19 16:05:35 +0300

    Implement options styles in user profile page

---
+++ WeLoveFood/Controllers/UserController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeLoveFood.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Orders()
+            => View();
+
+        public IActionResult PersonalData()
+            => View();
+    }
+}

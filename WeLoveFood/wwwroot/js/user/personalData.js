e3304e4b51ee7a59fc3294286631d5c25e25df05
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-21 22:34:40 +0300

    Implement personal data page styles

---
+++ WeLoveFood/wwwroot/js/user/personalData.js
@@ -0,0 +1,15 @@
+﻿(() => {
+    const change = (e) => {
+        e.target.parentElement.classList.add('hidden');
+
+        document.getElementById('save-btn')
+            .parentElement
+            .classList.remove('hidden');
+
+        document.querySelectorAll('input')
+            .forEach(e => e.classList.remove('disable'));
+    };
+
+    document.getElementById('change-btn')
+        .addEventListener('click', change);
+})();

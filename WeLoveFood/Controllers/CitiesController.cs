ec7df03fc09e4385cf03133267d169b119bef661
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-22 17:41:54 +0300

    Implement all cities button in index page and cities controller with all cities page

---
+++ WeLoveFood/Controllers/CitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeLoveFood.Services.Cities;
+
+namespace WeLoveFood.Controllers
+{
+    public class Cities : Controller
+    {
+        private readonly ICitiesService _cities;
+
+        public Cities(ICitiesService cities)
+            => this._cities = cities;
+
+        public IActionResult All()
+        {
+            var citiesCards = this._cities
+                .GetAllCitiesCardsOrderByRestaurantsCount();
+
+            return View(citiesCards);
+        }
+    }
+}

c52e4208b2c562c4ef496baebba6e5507bae202e
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-22 15:13:03 +0300

    Change navigation menu responsive styles

---
+++ WeLoveFood/wwwroot/js/shared/navMenu.js
@@ -0,0 +1,16 @@
+﻿(() => {
+    const activeClass = 'active';
+
+    const toggle = () => {
+        const buttons = document.getElementsByClassName('btns')[0];
+
+        if (buttons.classList.contains(activeClass)) {
+            buttons.classList.remove(activeClass);
+        } else {
+            buttons.classList.add(activeClass);
+        }
+    };
+
+    document.getElementsByClassName('fa-bars')[0]
+        .addEventListener('click', toggle);
+})();

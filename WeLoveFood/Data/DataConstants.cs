c5c433e70cd9c2adb80c19e599f051c1ed7f7cd0
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-22 23:12:04 +0300

    Create meal database table

--- WeLoveFood/Data/DataConstants.cs
+++ WeLoveFood/Data/DataConstants.cs
@@ -13,5 +13,16 @@
             public const int NameMinLength = 2;
             public const int NameMaxLength = 40;
         }
+
+        public class Meal
+        {
+            public const int NameMinLength = 3;
+            public const int NameMaxLength = 32;
+
+            public const int WeightMinValue = 10;
+            public const int WeightMaxValue = 1000;
+
+            public const int DescriptionMaxLength = 70;
+        }
     }
 }
---
+++ WeLoveFood/Data/Models/Meal.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using static WeLoveFood.Data.DataConstants.Meal;
+
+namespace WeLoveFood.Data.Models
+{
+    public class Meal
+    {
+        public int Id { get; init; }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string Name { get; set; }
+
+        [Range(WeightMinValue, WeightMaxValue)]
+        public int Weight { get; set; }
+
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        [Column(TypeName = "decimal(10,2)")]
+        public decimal Price { get; set; }
+    }
+}

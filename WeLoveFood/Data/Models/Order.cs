94651363664b56db527b6a9d175dafe603a62463
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-25 14:25:13 +0300

    Create order and portion database entities

---
+++ WeLoveFood/Data/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeLoveFood.Data.Models
+{
+    public class Order
+    {
+        public int Id { get; init; }
+
+        public DateTime Date { get; set; }
+
+        public int RestaurantId { get; set; }
+
+        public Restaurant Restaurant { get; set; }
+
+        public IEnumerable<Portion> Portions { get; init; } = new HashSet<Portion>();
+    }
+}
---
+++ WeLoveFood/Data/Models/Portion.cs
@@ -0,0 +1,13 @@
+﻿namespace WeLoveFood.Data.Models
+{
+    public class Portion
+    {
+        public int Id { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int MealId { get; set; }
+
+        public Meal Meal { get; set; }
+    }
+}

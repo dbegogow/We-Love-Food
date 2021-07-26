4325cd14c6ea2ebbfd356635dd20f7a6fb221196
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-07-26 22:17:02 +0300

    Implement claims principals extensions

---
+++ WeLoveFood/Infrastructure/ClaimsPrincipalsExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Claims;
+
+using static WeLoveFood.WebConstants;
+
+namespace WeLoveFood.Infrastructure
+{
+    public static class ClaimsPrincipalsExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+            => user.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        public static bool IsAdmin(this ClaimsPrincipal user)
+            => user.IsInRole(AdministratorRoleName);
+    }
+}

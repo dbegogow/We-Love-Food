02e4b19c139fad6f3b62c72e23d4839358fe266b
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-13 23:03:20 +0300

    Create manager area with base controller

---
+++ WeLoveFood/Areas/Manager/Controllers/ManagerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+using static WeLoveFood.WebConstants;
+
+namespace WeLoveFood.Areas.Manager.Controllers
+{
+    [Area(ManagerAreaName)]
+    [Authorize(Roles = ManagerRoleName)]
+    public abstract class ManagerController : Controller
+    {
+    }
+}

5f01bc6b18bc1617391e0ba400eb89cc8e83c668
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-17 10:06:33 +0300

    Create manager restaurant service model

---
+++ WeLoveFood/Services/Models/Restaurants/ManagerRestaurantServiceModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WeLoveFood.Services.Models.Restaurants
+{
+    public class ManagerRestaurantServiceModel
+    {
+        public int Id { get; init; }
+
+        public string Name { get; init; }
+
+        public bool IsApproved { get; init; }
+
+        public bool IsArchived { get; init; }
+    }
+}

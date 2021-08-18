5c8f2b1209ec41511431d66cd6c32f377540eb86
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-18 11:13:40 +0300

    Made manager restaurant menu comes from database

--- WeLoveFood/Areas/Manager/Controllers/MenusController.cs
+++ WeLoveFood/Areas/Manager/Controllers/MenusController.cs
@@ -1,14 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using WeLoveFood.Services.Menus;
+using WeLoveFood.Services.Managers;
+using WeLoveFood.Infrastructure.Extensions;
+using WeLoveFood.Models.Menus;
 
 namespace WeLoveFood.Areas.Manager.Controllers
 {
     public class MenusController : ManagerController
     {
+        private readonly IMenusService _menus;
+        private readonly IManagersService _managers;
+
+        public MenusController(
+            IMenusService menus,
+            IManagersService managers)
+        {
+            this._menus = menus;
+            this._managers = managers;
+        }
+
         public IActionResult Meals(int id)
         {
+            var hasRestaurant = this._managers
+                .HasRestaurant(User.Id(), id);
+
+            if (!hasRestaurant)
+            {
+                return BadRequest();
+            }
+
+            var menu = new List<ManagerMenuViewModel>();
+
+            var mealsCategories = this._menus
+                .RestaurantMealsCategories(id);
+
+            foreach (var mealsCategory in mealsCategories)
+            {
+                var meals = this._menus
+                    .MealsCategory(mealsCategory.Id)
+                    .ToList();
+
+                menu.Add(new ManagerMenuViewModel
+                {
+                    MealsCategory = mealsCategory,
+                    Meals = meals
+                });
+            }
+
             ViewBag.RestaurantId = id;
 
-            return View();
+            return View(menu);
         }
     }
 }
---
+++ WeLoveFood/Models/Menus/ManagerMenuViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WeLoveFood.Services.Models.Menus;
+
+namespace WeLoveFood.Models.Menus
+{
+    public class ManagerMenuViewModel
+    {
+        public CategoryServiceModel MealsCategory { get; init; }
+
+        public IEnumerable<MealServiceModel> Meals { get; init; }
+    }
+}

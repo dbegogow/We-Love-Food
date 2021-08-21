3fb01a0ae32a1207bb05b07ef26c8b3f16d9b73f
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-21 09:56:43 +0300

    Add cities controller tests

---
+++ WeLoveFood.Test/Controllers/CitiesControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using Xunit;
+using System;
+using WeLoveFood.Controllers;
+using MyTested.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using WeLoveFood.Services.Models.Cities;
+using static WeLoveFood.CacheConstants;
+using static WeLoveFood.Test.Data.Cities;
+
+namespace WeLoveFood.Test.Controllers
+{
+    public class CitiesControllerTest
+    {
+        [Fact]
+        public void AllShouldReturnCorrectViewWithCities()
+            => MyController<CitiesController>
+                .Instance(controller => controller
+                    .WithData(FiveCities()))
+                .Calling(c => c.All())
+                .ShouldHave()
+                .MemoryCache(cache => cache
+                    .ContainingEntry(entry => entry
+                        .WithKey(AllCitiesCardsCacheKey)
+                        .WithAbsoluteExpirationRelativeToNow(TimeSpan.FromMinutes(30))
+                        .WithValueOfType<List<CityCardServiceModel>>()))
+                .AndAlso()
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<List<CityCardServiceModel>>()
+                    .Passing(model => model.Count == 5));
+    }
+}
---
+++ WeLoveFood.Test/Data/Cities.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+using WeLoveFood.Data.Models;
+using System.Collections.Generic;
+
+namespace WeLoveFood.Test.Data
+{
+    public static class Cities
+    {
+        public static IEnumerable<City> FiveCities()
+            => Enumerable.Range(0, 5).Select(i => new City());
+    }
+}

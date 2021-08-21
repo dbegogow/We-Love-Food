420d80ed824ae876f06ff3bbd3cf7c00e5542c80
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-21 10:31:56 +0300

    Add menus controller tests

---
+++ WeLoveFood.Test/Controllers/MenusControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using MyTested.AspNetCore.Mvc;
+using WeLoveFood.Controllers;
+using WeLoveFood.Models.Menus;
+using Xunit;
+
+using static WeLoveFood.Test.Data.Menus;
+using static WeLoveFood.Test.Data.Restaurants;
+
+namespace WeLoveFood.Test.Controllers
+{
+    public class MenusControllerTest
+    {
+        [Theory]
+        [InlineData(1, 1)]
+        public void MealsShouldReturnCorrectViewWithValidModel(
+            int restaurantId,
+            int categoryId)
+            => MyController<MenusController>
+                .Instance(instance => instance
+                    .WithData( Meals()))
+                .Calling(c => c.Meals(restaurantId, categoryId))
+                .ShouldReturn()
+                .View(view => view
+                    .WithModelOfType<MenuViewModel>()
+                    .Passing(menu => menu.Meals.Count() == 1));
+    }
+}
---
+++ WeLoveFood.Test/Data/Menus.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using WeLoveFood.Data.Models;
+using System.Collections.Generic;
+
+using static WeLoveFood.Test.Data.Restaurants;
+
+namespace WeLoveFood.Test.Data
+{
+    public static class Menus
+    {
+        public static IEnumerable<Meal> Meals()
+        {
+            var restaurants = FiveRestaurants();
+
+            var mealsCategories = new List<MealsCategory>
+            {
+                new() {RestaurantId = 1},
+                new() {RestaurantId = 2}
+            };
+
+            var meals = new List<Meal>
+            {
+                new() {MealsCategoryId = 1},
+                new() {MealsCategoryId = 2}
+            };
+
+            return meals;
+        }
+    }
+}

d530feb839fa5b0cee7f2fe63afc699126a198de
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-21 09:35:39 +0300

    Add controller test to home controller

---
+++ WeLoveFood.Test/Controllers/HomeControllerTest.cs
@@ -0,0 +1,58 @@
+﻿using Xunit;
+using WeLoveFood.Controllers;
+using MyTested.AspNetCore.Mvc;
+using RestaurantsController = WeLoveFood.Areas.Manager.Controllers.RestaurantsController;
+using OrdersController = WeLoveFood.Areas.Waiter.Controllers.OrdersController;
+
+using static WeLoveFood.WebConstants;
+
+namespace WeLoveFood.Test.Controllers
+{
+    public class HomeControllerTest
+    {
+        [Fact]
+        public void IndexShouldReturnViewWhenUserIsClient()
+            => MyController<HomeController>
+                .Instance(controller => controller
+                    .WithUser("Client", new[] { ClientRoleName }))
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void IndexShouldReturnViewWenNoUser()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .View();
+
+        [Fact]
+        public void IndexShouldRedirectToManagerRestaurantsControllerWhenUserIsManager()
+            => MyController<HomeController>
+                .Instance(controller => controller
+                    .WithUser("Manager", new[] { ManagerRoleName }))
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<RestaurantsController>(c => c.Mine()));
+
+        [Fact]
+        public void IndexShouldRedirectToWaiterOrdersControllerWhenUserIsWaiter()
+            => MyController<HomeController>
+                .Instance(controller => controller
+                    .WithUser("Waiter", new[] { WaiterRoleName }))
+                .Calling(c => c.Index())
+                .ShouldReturn()
+                .Redirect(redirect => redirect
+                    .To<OrdersController>(c => c.All()));
+
+        [Fact]
+        public void ErrorShouldReturnView()
+            => MyController<HomeController>
+                .Instance()
+                .Calling(c => c.Error())
+                .ShouldReturn()
+                .View();
+    }
+}

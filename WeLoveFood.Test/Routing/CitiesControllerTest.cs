1c83a017a0a2e7d608213ebe00e8cf2016453c5f
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-08-21 10:04:19 +0300

    Add cities controller routing tests

---
+++ WeLoveFood.Test/Routing/CitiesControllerTest.cs
@@ -0,0 +1,16 @@
+﻿using MyTested.AspNetCore.Mvc;
+using WeLoveFood.Controllers;
+using Xunit;
+
+namespace WeLoveFood.Test.Routing
+{
+    public class CitiesControllerTest
+    {
+        [Fact]
+        public void GetBecomeShouldBeMapped()
+            => MyRouting
+                .Configuration()
+                .ShouldMap("/Cities")
+                .To<CitiesController>(c => c.All());
+    }
+}
